e431582d03cea2d174817ea11d86f00e0ff8da8b
ahwet-sen <ahwt93sn@gmail.com>
2025-06-19 14:40:59 +0300

    v1.6.0

--- src/AssetFiles.php
+++ src/AssetFiles.php
@@ -14,7 +14,7 @@ final class AssetFiles
     /**
      * Package version.
      */
-    private const PACKAGE_VERSION = 'v1.5.0';
+    private const PACKAGE_VERSION = 'v1.6.0';
 
     /**
      * Package name.
