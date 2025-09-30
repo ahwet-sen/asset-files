50bfb0b2f304c1fcb9c80bd45f9b263812925775
Ahwet Şen <ahwt93sn@gmail.com>
2025-09-30 15:03:22 +0300

    Update AssetFiles.php

--- src/AssetFiles.php
+++ src/AssetFiles.php
@@ -14,7 +14,7 @@ final class AssetFiles
     /**
      * Package version.
      */
-    private const PACKAGE_VERSION = 'v1.6.0';
+    private const PACKAGE_VERSION = 'v1.8.0';
 
     /**
      * Package name.
